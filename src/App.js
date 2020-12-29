118d5244dceccde6683a18df306112117be6a788
xiaobai5916 <961230564@qq.com>
2020-12-29 09:36:20 +0800

    'msg'

---
+++ src/App.js
@@ -0,0 +1,12 @@
+/*
+ 应用的根组件
+*/
+import React, {component} from 'react';
+
+export default class App extends component {
+    render () {
+        return (
+            <div>App</div>
+        )
+    }
+}
