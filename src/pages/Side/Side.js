dd8b39771eb86f67f73d56b76a36c0601a8f4aad
xiaobai5916 <961230564@qq.com>
2021-01-13 11:05:08 +0800

    'side修改'

--- src/pages/Side/Side.js
+++ src/pages/Side/Side.js
@@ -1,40 +1,95 @@
 import './Side.css';
+import React from 'react';
 import { Layout, Menu } from 'antd';
 import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
 
 const { Sider } = Layout;
 const { SubMenu } = Menu;
 
-function Side () {
-    return (
-        <Sider width={200} className="site-layout-background">
-            <Menu
-                mode="inline"
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={['sub1']}
-                style={{ height: '100%', borderRight: 0 }}
-                >
-                <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-                    <Menu.Item key="1">option1</Menu.Item>
-                    <Menu.Item key="2">option2</Menu.Item>
-                    <Menu.Item key="3">option3</Menu.Item>
-                    <Menu.Item key="4">option4</Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-                    <Menu.Item key="5">option5</Menu.Item>
-                    <Menu.Item key="6">option6</Menu.Item>
-                    <Menu.Item key="7">option7</Menu.Item>
-                    <Menu.Item key="8">option8</Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
-                    <Menu.Item key="9">option9</Menu.Item>
-                    <Menu.Item key="10">option10</Menu.Item>
-                    <Menu.Item key="11">option11</Menu.Item>
-                    <Menu.Item key="12">option12</Menu.Item>
-                </SubMenu>
-            </Menu>
-        </Sider>
-    )
-}
+class Side extends React.Component {
 
+    state = {
+      data: [
+        {
+          "key": '1',
+          "title": "测试",
+          "children": [
+            {
+              "key": '4',
+              "title": "测试4",
+              "children": [
+                {
+                  "key": '7',
+                  "title": "测试7"
+                },
+                {
+                  "key": '8',
+                  "title": "测试8"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "key": '2',
+          "title": "测试2",
+          "children": [
+            {
+              "key": '5',
+              "title": "测试5",
+            }
+          ]
+        },
+        {
+          "key": '3',
+          "title": "测试3",
+          "children": [
+            {
+              "key": '6',
+              "title": "测试6",
+              "children": [
+                {
+                  "key": '9',
+                  "title": "测试9",
+                },
+                {
+                  "key": '10',
+                  "title": "测试10",
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  
+    recursion(dataSource) {
+      return (
+        dataSource.map((menu, index) => {
+          if (menu.children) {
+            return (
+              <SubMenu key={menu.key} title={menu.title}>
+                {this.recursion(menu.children)}
+              </SubMenu>
+            )
+          } else {
+            return (<Menu.Item key={menu.key}>{menu.title}</Menu.Item>)
+          }
+        })
+      )
+    }
+  
+    render() {
+      return (
+        <Sider width={200} className="site-layout-background">
+          <Menu
+            mode="inline"
+            style={{ width: 200 }}
+          >
+            { this.recursion(this.state.data) }
+          </Menu>
+        </Sider>
+      );
+    }
+  }
 export default Side;
